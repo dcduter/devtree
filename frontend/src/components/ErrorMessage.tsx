bc03d869ce59f81c9a410ac0ba1e37b0ebc1a7a8
Diego <diegonovoa001@gmail.com>
2025-10-10 03:51:20 -0500

    se borro frontend

--- frontend/src/components/ErrorMessage.tsx
+++
@@ -1,8 +0,0 @@
-type ErrorMessageProps = {
-    children: React.ReactNode
-}
-export default function ErrorMessage({children} : ErrorMessageProps) {
-  return (
-    <p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold text-center">{children}</p>
-  )
-}
--- frontend/src/views/NotFoundView.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function NotFoundView() {
-  return (
-    <p className="font-bold text-2xl text-center text-white">Usuario No Encontrado</p>
-  )
-}
